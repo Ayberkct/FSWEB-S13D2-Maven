7b55f87e42ffd7315b6c6017445efa92f42964d9
Ayberk <ayberkcetinkaya54@gmail.com>
2024-09-22 19:55:56 +0300

    com guncel

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,82 +1,80 @@
 package org.example;
 
 public class Main {
+    public static  void main(String[] args){
+       // System.out.println(isPalindrome(-1221));
+        // System.out.println(isPerfectNumber(25));
+        System.out.println(numberToWords(123));
+    }
     public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        char[] digits = String.valueOf(number).toCharArray();
 
-        int originalNumber = Math.abs(number);
-        int reversedNumber = 0;
-        int temp = originalNumber;
+        String reversed = "";
 
-        while (temp > 0) {
-            int lastDigit = temp % 10;
-            reversedNumber = (reversedNumber * 10) + lastDigit;
-            temp /= 10;
+        for(int i=digits.length-1; i>=0 ; i--){
+            reversed = reversed + digits[i];
         }
 
-
-        return originalNumber == reversedNumber;
+        return reversed.equalsIgnoreCase(String.valueOf(number)) ;
     }
-    public static boolean isPerfectNumber(int number) {
-
-        if (number < 1) {
-            return false;
+    public  static boolean isPerfectNumber(int number){
+      if(number<0)
+          return false;
+    int total = 0;
+    for(int i=1; i <= number / 2 ; i++){
+        if(number % i == 0) {
+            total = total + i;
         }
-
-        int sum = 0;
-
-        for (int i = 1; i <= number / 2; i++) {
-            if (number % i == 0) {
-                sum += i;
-            }
-        }
-
-        return sum == number;
     }
+    if(total==number){
+        return true;
+    }else{
+        return false;
+    }
+    }
+    public static String numberToWords(int number){
 
-    public static String numberToWords(int number) {
-        if (number < 0) {
-
+        if(number<0)
             return "Invalid Value";
-        }
 
-        int reversedNumber = 0;
-        int digitCount = 0;
-
-
-        int temp = number;
-        while (temp > 0) {
-            int lastDigit = temp % 10;
-            reversedNumber = (reversedNumber * 10) + lastDigit;
-            temp /= 10;
-            digitCount++;
-        }
-
-        while (reversedNumber > 0) {
-            int digit = reversedNumber % 10;
-            switch (digit) {
-                case 0: System.out.print("Zero "); break;
-                case 1: System.out.print("One "); break;
-                case 2: System.out.print("Two "); break;
-                case 3: System.out.print("Three "); break;
-                case 4: System.out.print("Four "); break;
-                case 5: System.out.print("Five "); break;
-                case 6: System.out.print("Six "); break;
-                case 7: System.out.print("Seven "); break;
-                case 8: System.out.print("Eight "); break;
-                case 9: System.out.print("Nine "); break;
+        char[] digits = String.valueOf(number).toCharArray();
+            String numToText = "";
+        for(char digit : digits){
+            switch (digit){
+                case '0':
+                  numToText +=  "Zero ";
+                  break;
+                case '1':
+                    numToText +=  "One ";
+                    break;
+                case '2':
+                    numToText +=  "Two ";
+                    break;
+                case '3':
+                    numToText +=  "Three ";
+                    break;
+                case '4':
+                    numToText +=  "Four ";
+                    break;
+                case '5':
+                    numToText +=  "Five ";
+                    break;
+                case '6':
+                    numToText +=  "Six ";
+                    break;
+                case '7':
+                    numToText +=  "Seven ";
+                    break;
+                case '8':
+                    numToText +=  "Eight ";
+                    break;
+                case '9':
+                    numToText +=  "Nine ";
+                    break;
             }
-            reversedNumber /= 10;
+
         }
-
-
-        while (digitCount > 0 && number % 10 == 0) {
-            System.out.print("Zero ");
-            digitCount--;
-            number /= 10;
-        }
-
-
+        return  numToText.trim();
     }
-
-    }
-
+}
